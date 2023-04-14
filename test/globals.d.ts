c1b59cffb50b29ca830a94dab1de7597006d200c
Victor Nathanael <victornathanael15@gmail.com>
2023-04-14 10:09:40 -0300

    add export

--- test/globals.d.ts
+++ test/globals.d.ts
@@ -1,5 +1,5 @@
-import { SuperTest, Test } from 'supertest';
-
 declare global {
-  var testRequest: SuperTest<Test>;
+  var testRequest: import('supertest').SuperTest<import('supertest').Test>;
 }
+
+export {};
