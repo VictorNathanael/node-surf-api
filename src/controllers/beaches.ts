663c9f53adcf99ce7e2cd586e1c45ce244e5302f
Victor Nathanael <victornathanael15@gmail.com>
2023-04-20 11:09:14 -0300

    feat: add authMiddleware on controller

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,22 +1,27 @@
-import { Controller, Post } from '@overnightjs/core';
+import { Controller, Post, ClassMiddleware } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { Beach } from '@src/models/beach';
 import mongoose from 'mongoose';
+import { authMiddleware } from '@src/middlewares/auth';
 
 @Controller('beaches')
+@ClassMiddleware(authMiddleware)
 export class BeachesController {
-  @Post('')
-  public async create(req: Request, res: Response): Promise<void> {
-    try {
-      const beach = new Beach(req.body);
-      const result = await beach.save();
-      res.status(201).send(result);
-    } catch (error) {
-      if (error instanceof mongoose.Error.ValidationError) {
-        res.status(422).send({ error: error.message });
-      } else {
-        res.status(500).send({ error: 'Internal Server Error' });
-      }
+    @Post('')
+    public async create(req: Request, res: Response): Promise<void> {
+        try {
+            const beach = new Beach({
+                ...req.body,
+                ...{ user: req.decoded?.id },
+            });
+            const result = await beach.save();
+            res.status(201).send(result);
+        } catch (error) {
+            if (error instanceof mongoose.Error.ValidationError) {
+                res.status(422).send({ error: error.message });
+            } else {
+                res.status(500).send({ error: 'Internal Server Error' });
+            }
+        }
     }
-  }
 }
