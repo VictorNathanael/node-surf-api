83d636cc55b93eda7b5ac33138c52b54c31dd60a
Victor Nathanael <victornathanael15@gmail.com>
2023-04-20 11:14:11 -0300

    test: add defaultUser and set the token for authentication

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,37 +1,57 @@
 import { Beach } from '@src/models/beach';
+import { User } from '@src/models/user';
+import AuthService from '@src/services/auth';
 
 describe('Beaches functional tests', () => {
-  beforeAll(async () => await Beach.deleteMany({}));
-  describe('When creating a new beach', () => {
-    it('should create a beach with success', async () => {
-      const newBeach = {
-        lat: -33.792726,
-        lng: 151.289824,
-        name: 'Manly',
-        position: 'E',
-      };
+    const defaultUser = {
+        name: 'John Doe',
+        email: 'john2@mail.com',
+        password: '1234',
+    };
 
-      const response = await global.testRequest.post('/beaches').send(newBeach);
-      expect(response.status).toBe(201);
-      expect(response.body).toEqual(expect.objectContaining(newBeach));
+    let token: string;
+    beforeEach(async () => {
+        await Beach.deleteMany({});
+        await User.deleteMany({});
+        const user = await new User(defaultUser).save();
+        token = AuthService.generateToken(user.toJSON());
     });
 
-    it('should return 422 when there is a validation error', async () => {
-      const newBeach = {
-        lat: 'invalid_string',
-        lng: 151.289824,
-        name: 'Manly',
-        position: 'E',
-      };
-      const response = await global.testRequest.post('/beaches').send(newBeach);
+    describe('When creating a new beach', () => {
+        it('should create a beach with success', async () => {
+            const newBeach = {
+                lat: -33.792726,
+                lng: 151.289824,
+                name: 'Manly',
+                position: 'E',
+            };
 
-      expect(response.status).toBe(422);
-      expect(response.body).toEqual({
-        error:
-          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" (type string) at path "lat"',
-      });
+            const response = await global.testRequest
+                .post('/beaches')
+                .set({ 'x-access-token': token })
+                .send(newBeach);
+            expect(response.status).toBe(201);
+            expect(response.body).toEqual(expect.objectContaining(newBeach));
+        });
+
+        it('should return 422 when there is a validation error', async () => {
+            const newBeach = {
+                lat: 'invalid_string',
+                lng: 151.289824,
+                name: 'Manly',
+                position: 'E',
+            };
+            const response = await global.testRequest
+                .post('/beaches')
+                .set({ 'x-access-token': token })
+                .send(newBeach);
+
+            expect(response.status).toBe(422);
+            expect(response.body).toEqual({
+                error: 'Beach validation failed: lat: Cast to Number failed for value "invalid_string" (type string) at path "lat"',
+            });
+        });
+
+        it.skip('should return 500 when there is any error other than validation error', async () => {});
     });
-
-    it.skip('should return 500 when there is any error other than validation error', async () => {});
-  });
 });
