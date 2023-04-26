f64310ec41bb353679fb6997ff91cb73c503a251
Victor Nathanael <victornathanael15@gmail.com>
2023-04-26 08:48:17 -0300

    add logger.info

--- src/server.ts
+++ src/server.ts
@@ -6,49 +6,50 @@ import { ForecastController } from './controllers/forecast';
 import * as database from '@src/database';
 import { BeachesController } from './controllers/beaches';
 import { UsersController } from './controllers/users';
+import logger from './logger';
 
 export class SetupServer extends Server {
-  constructor(private port = 3000) {
-    super();
-  }
+    constructor(private port = 3000) {
+        super();
+    }
 
-  public async init(): Promise<void> {
-    this.setupExpress();
-    this.setupControllers();
-    await this.databaseSetup();
-  }
+    public async init(): Promise<void> {
+        this.setupExpress();
+        this.setupControllers();
+        await this.databaseSetup();
+    }
 
-  private setupExpress(): void {
-    this.app.use(bodyParser.json());
-    this.setupControllers();
-  }
+    private setupExpress(): void {
+        this.app.use(bodyParser.json());
+        this.setupControllers();
+    }
 
-  private setupControllers(): void {
-    const forecastController = new ForecastController();
-    const beachesController = new BeachesController();
-    const usersController = new UsersController();
-    this.addControllers([
-      forecastController,
-      beachesController,
-      usersController,
-    ]);
-  }
+    private setupControllers(): void {
+        const forecastController = new ForecastController();
+        const beachesController = new BeachesController();
+        const usersController = new UsersController();
+        this.addControllers([
+            forecastController,
+            beachesController,
+            usersController,
+        ]);
+    }
 
-  public getApp(): Application {
-    return this.app;
-  }
+    public getApp(): Application {
+        return this.app;
+    }
 
-  private async databaseSetup(): Promise<void> {
-    await database.connect();
-  }
+    private async databaseSetup(): Promise<void> {
+        await database.connect();
+    }
 
-  public async close(): Promise<void> {
-    await database.close();
-  }
+    public async close(): Promise<void> {
+        await database.close();
+    }
 
-  public start(): void {
-    this.app.listen(this.port, () => {
-      console.info('Server listening on port:', this.port);
-    });
-  }
+    public start(): void {
+        this.app.listen(this.port, () => {
+            logger.info('Server listening on port: ' + this.port);
+        });
+    }
 }
